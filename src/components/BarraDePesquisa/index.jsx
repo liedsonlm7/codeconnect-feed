b06d295ac500a647a2922cebd0a94d1c30b6fe51
liedsonlm7 <liedsonlm7@gmail.com>
2025-05-23 15:36:53 -0300

    versao inicial

---
+++ src/components/BarraDePesquisa/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './styles.css';
+
+export default function BarraDePesquisa() {
+    const [termoPesquisa, setTermoPesquisa] = useState(''); 
+    console.log(termoPesquisa);
+    return (
+        <input 
+            type="search" 
+            placeholder="Digite o que você procura" 
+            className="barra-pesquisa"
+            value={termoPesquisa}
+            onChange={(e) => setTermoPesquisa(e.target.value)} />
+    )
+}
